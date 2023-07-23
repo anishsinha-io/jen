be9a43988b1d9541edb7b31394e31dddbee267f9
Anish Sinha <anishsinha0128@gmail.com>
2023-07-22 21:17:20 -0400

    Refactor handleKeyDown function to be more explicit and concise

--- client/src/components/editor/keybindings/meta.ts
+++ client/src/components/editor/keybindings/meta.ts
@@ -1,32 +1,11 @@
 import { KeyboardEvent } from "react";
 import { Editor } from "slate";
 import { toggleMark } from "@components/editor/util/marks";
-
-export const keybindingMap = {
-    CMD_BOLD: "b",
-    CMD_ITALIC: "i",
-    CMD_UNDERLINE: "u",
-    CMD_STRIKETHROUGH: "x",
-};
+import isHotkey from "is-hotkey";
 
 export const handleKeyDown = (editor: Editor, e: KeyboardEvent<HTMLDivElement>) => {
-    if (e.metaKey) {
-        e.preventDefault();
-        switch (e.key) {
-            case keybindingMap.CMD_BOLD:
-                toggleMark(editor, "bold");
-                break;
-            case keybindingMap.CMD_ITALIC:
-                toggleMark(editor, "italic");
-                break;
-            case keybindingMap.CMD_UNDERLINE:
-                toggleMark(editor, "underline");
-                break;
-            case keybindingMap.CMD_STRIKETHROUGH:
-                if (e.shiftKey) {
-                    toggleMark(editor, "strikethrough");
-                }
-                break;
-        }
-    }
+    if (isHotkey("mod+b", e)) toggleMark(editor, "bold");
+    if (isHotkey("mod+i", e)) toggleMark(editor, "italic");
+    if (isHotkey("mod+u", e)) toggleMark(editor, "underline");
+    if (isHotkey("mod+shift+x", e)) toggleMark(editor, "strikethrough");
 };
